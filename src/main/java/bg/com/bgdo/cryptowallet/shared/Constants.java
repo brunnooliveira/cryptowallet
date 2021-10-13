45b6fea4873d72d12dc990683559f00bf3d26ef8
brunnooliveira <brunno.oliveira-ext@viavarejo.com.br>
2021-10-12 21:18:14 -0300

    feat: adjust security config for frontend

--- src/main/java/bg/com/bgdo/cryptowallet/shared/Constants.java
+++ src/main/java/bg/com/bgdo/cryptowallet/shared/Constants.java
@@ -1,7 +1,7 @@
 package bg.com.bgdo.cryptowallet.shared;
 
 public class Constants {
-  final public static String URL_API_BASE_PUBLIC = "/api/public";
+  final public static String URL_API_BASE_PUBLIC = "/public/api";
   final public static String URL_API_BASE_PRIVATE = "/api";
   final public static String URL_SWAGGER = "/swagger-ui";
   final public static String URL_API_DOCS = "/v3/api-docs";
